02a2bda0afa84813600d7e3fc58d1dac9f297ffe
EduardoLob <eduardohenriquelobo11@gmail.com>
2021-11-22 18:23:50 -0300

    criação da rota para o login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdministradorListComponent } from './components/administrador/administrador-list/administrador-list.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ResultadosComponent } from './components/resultados/resultados.component';
 
 const routes: Routes = [
+  {path: 'login', component:LoginComponent},
   {path: '', component:NavComponent, children: [
     {path: 'home', component:HomeComponent},
     {path: 'adm', component:AdministradorListComponent}
